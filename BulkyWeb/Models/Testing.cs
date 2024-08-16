ae9de09987ed31e97fdafb99f22bb723b2f2d2db
MOHAMMAD ANAS <92999181+MohammadAnas15@users.noreply.github.com>
2024-08-16 20:33:24 +0500

    toFirst

---
+++ BulkyWeb/Models/Testing.cs
@@ -0,0 +1,15 @@
+﻿namespace BulkyWeb.Models
+{
+    public class Testing
+    {
+        public int ID { get; set; }
+
+        public int age { get; set; } = 0;
+
+        public int? length { get; set; } = null;
+
+        public string? Namee { get; set; } = "abc";
+
+        public string detail { get; set; } = "carton";
+    }
+}
